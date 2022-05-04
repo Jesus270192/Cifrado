27cf1cda6f6447e28e59f633af180422fa0602a4
Jesús Antonio Rodríguez Sánchez <jesus_rodriguez@ANPER-DESAR-L35>
2022-05-04 13:45:07 -0500

    Nuevo proyecto

---
+++ GetJasonValues/GetJasonValues/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GetJasonValues {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            TextRange textRange = new TextRange(
+                        // TextPointer to the start of content in the RichTextBox.
+                        rtbJSONOriginal.Document.ContentStart,
+                        // TextPointer to the end of content in the RichTextBox.
+                        rtbJSONOriginal.Document.ContentEnd
+                    );
+
+            try {
+                JObject DynamicData = (JObject)JsonConvert.DeserializeObject(textRange.Text);
+                
+
+                dynamic details = JObject.Parse(textRange.Text);
+                JsonElement root = details.RootElement;
+            } catch (Exception ex) {
+
+            }
+
+
+
+        }
+    }
+}
